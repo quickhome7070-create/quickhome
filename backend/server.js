130fb5ad4febb089ea4680b070518b60d237ddda
jayingale2506 <jayingale.2506@gmail.com>
2026-03-26 17:02:20 +0530

    update cors for production

--- backend/server.js
+++ backend/server.js
@@ -26,7 +26,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:3000", "https://your-app.vercel.app",],
+    origin: ["http://localhost:3000", "https://quickhome.vercel.app",],
     credentials: true,
     methods: ["GET", "POST"],
   },
