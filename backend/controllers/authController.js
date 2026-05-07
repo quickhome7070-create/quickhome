7014f1f8ccfa5326310aaf0fe063ab8297bf0cb5
jayingale2506 <jayingale.2506@gmail.com>
2026-05-07 17:32:57 +0530

    resend issue

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -178,8 +178,8 @@ exports.forgotPassword = async (req, res) => {
 
     // FRONTEND URL
     // const resetUrl = `http://localhost:3000/reset-password/${resetToken}`;
-    // const resetUrl = `https://www.ghardestiny.com/reset-password/${resetToken}`;
-    const resetUrl = `${process.env.CLIENT_URL}/reset-password/${resetToken}`;
+   const resetUrl =
+ `${process.env.FRONTEND_URL}/reset-password/${resetToken}`;
  
 
     await sendEmail({
