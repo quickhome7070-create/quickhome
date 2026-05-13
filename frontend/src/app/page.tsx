6ea99d2d6b5349a3e0a6f26b2ff09c1b7fbe7985
jayingale2506 <jayingale.2506@gmail.com>
2026-05-13 22:50:35 +0530

    added ISR

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,22 +1,42 @@
-import { Suspense } from "react";
+// app/page.tsx
 
 import PropertiesClient from "./properties/PropertiesClient";
 
+async function getProperties() {
 
-export default function HomePage() {
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_API_URL}/property`,
+    {
+      next: {
+        revalidate: 60,
+      },
+    }
+  );
+
+  const data =
+    await res.json();
+
+  return data.properties || [];
+}
+
+export default async function HomePage() {
+
+  const properties =
+    await getProperties();
+
   return (
     <main className="min-h-screen bg-gray-50">
+
       <div className="max-w-7xl mx-auto p-6">
 
         <h1 className="text-3xl font-bold text-center mb-6">
           Welcome to the fastest home search destination
         </h1>
 
-        <Suspense fallback={<p>Loading</p>}>
-          <PropertiesClient
-            searchParams={{}}
-          />
-        </Suspense>
+        <PropertiesClient
+  initialProperties={properties}
+  searchParams={{}}
+/>
 
       </div>
     </main>
