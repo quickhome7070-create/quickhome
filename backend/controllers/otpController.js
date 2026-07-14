0bbc4380eb13ff1b2a975c9592717cbeabe94c72
jayingale2506 <jayingale.2506@gmail.com>
2026-07-14 20:06:40 +0530

    clean code

--- backend/controllers/otpController.js
+++ backend/controllers/otpController.js
@@ -9,6 +9,28 @@ const MAX_ATTEMPTS = 5;
 const BLOCK_WINDOW_MINUTES = 10;
 const MAX_BLOCKED_OTPS = 3;
 
+exports.loginWithMSG91 = async (req,res)=>{
 
+  try {
+
+    const { phone } = req.body;
+
+    console.log("MSG91 LOGIN PHONE:", phone);
+
+
+    res.json({
+      message:"Login successful"
+    });
+
+
+  } catch(error){
+
+    res.status(500).json({
+      message:error.message
+    });
+
+  }
+
+};
 
 
